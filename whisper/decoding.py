fc406b7b2b64e2c580ddfae26bedc8f673b091e4
Rio Harper <50555644+rioharper@users.noreply.github.com>
2022-12-11 22:25:33 -0500

    Update decoding.py

--- whisper/decoding.py
+++ whisper/decoding.py
@@ -437,7 +437,7 @@ class ApplyTimestampRules(LogitFilter):
         for k in range(tokens.shape[0]):
             timestamp_logprob = logprobs[k, self.tokenizer.timestamp_begin :].logsumexp(dim=-1)
             max_text_token_logprob = logprobs[k, : self.tokenizer.timestamp_begin].max()
-            if timestamp_logprob > max_text_token_logprob:
+            if timestamp_logprob > max_text_token_logprob * 10:
                 logits[k, : self.tokenizer.timestamp_begin] = -np.inf
 
 
